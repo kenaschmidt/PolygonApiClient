cd74793946929089fc90daaa809e7c8523d68492
Ken Schmidt <kenaschmidt@gmail.com>
2024-01-02 10:04:34 -0600

    Updated to include asset class access permissions for subscription levels

--- MasterClient/PolygonSubscriptionSettings.cs
+++ MasterClient/PolygonSubscriptionSettings.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -29,26 +30,175 @@ namespace PolygonApiClient.MasterClient
             Currencies = new CurrenciesSettings(currencies);
         }
 
+        public AssetClassSettings GetSettings(PolygonConnectionEndpoint endpoint)
+        {
+            switch (endpoint)
+            {
+                case PolygonConnectionEndpoint.stocks:
+                    return Stocks;
+                case PolygonConnectionEndpoint.options:
+                    return Options;
+                case PolygonConnectionEndpoint.indices:
+                    return Indices;
+                case PolygonConnectionEndpoint.forex:
+                case PolygonConnectionEndpoint.crypto:
+                    return Currencies;
+                default:
+                    return null;
+            }
+        }
     }
 
     public abstract class AssetClassSettings
     {
-        public PolygonSubscription Subsription { get; private set; }
+        public PolygonSubscription Subscription { get; private set; }
 
         public abstract DateTime HistoricalDataLookback { get; }
+        public PolygonPermissions Permissions { get; private set; }
 
         public AssetClassSettings(PolygonSubscription subscription)
         {
-            Subsription = subscription;
+            Subscription = subscription;
+
+            setPermissionFlags();
+        }
+
+        private void setPermissionFlags()
+        {
+            //
+            // Stocks
+            //
+            if (this.GetType() == typeof(StocksSettings))
+            {
+                switch (Subscription)
+                {
+                    case PolygonSubscription.basic:
+                        Permissions =
+                            PolygonPermissions.None;
+                        break;
+                    case PolygonSubscription.starter:
+                        Permissions =
+                            PolygonPermissions.Snapshots |
+                            PolygonPermissions.WebSockets;
+                        break;
+                    case PolygonSubscription.developer:
+                        Permissions =
+                            PolygonPermissions.Snapshots |
+                            PolygonPermissions.WebSockets |
+                            PolygonPermissions.Trades;
+                        break;
+                    case PolygonSubscription.advanced:
+                        Permissions =
+                            PolygonPermissions.Snapshots |
+                            PolygonPermissions.WebSockets |
+                            PolygonPermissions.Trades |
+                            PolygonPermissions.Quotes;
+                        break;
+                    default:
+                        Permissions =
+                            PolygonPermissions.None;
+                        break;
+                }
+            }
+
+            //
+            // Options
+            //
+            if (this.GetType() == typeof(OptionsSettings))
+            {
+                switch (Subscription)
+                {
+                    case PolygonSubscription.basic:
+                        Permissions =
+                            PolygonPermissions.None;
+                        break;
+                    case PolygonSubscription.starter:
+                        Permissions =
+                            PolygonPermissions.Snapshots |
+                            PolygonPermissions.WebSockets;
+                        break;
+                    case PolygonSubscription.developer:
+                        Permissions =
+                            PolygonPermissions.Snapshots |
+                            PolygonPermissions.WebSockets |
+                            PolygonPermissions.Trades;
+                        break;
+                    case PolygonSubscription.advanced:
+                        Permissions =
+                            PolygonPermissions.Snapshots |
+                            PolygonPermissions.WebSockets |
+                            PolygonPermissions.Trades |
+                            PolygonPermissions.Quotes;
+                        break;
+                    default:
+                        Permissions =
+                            PolygonPermissions.None;
+                        break;
+                }
+            }
+
+            //
+            // Indices
+            //
+            if (this.GetType() == typeof(IndicesSettings))
+            {
+                switch (Subscription)
+                {
+                    case PolygonSubscription.basic:
+                        Permissions =
+                            PolygonPermissions.None;
+                        break;
+                    case PolygonSubscription.starter:
+                    case PolygonSubscription.advanced:
+                        Permissions =
+                            PolygonPermissions.Snapshots |
+                            PolygonPermissions.WebSockets;
+                        break;
+                    default:
+                        Permissions =
+                            PolygonPermissions.None;
+                        break;
+                }
+            }
+
+            //
+            // Currencies
+            //
+            if (this.GetType() == typeof(CurrenciesSettings))
+            {
+                switch (Subscription)
+                {
+                    case PolygonSubscription.basic:
+                        Permissions =
+                            PolygonPermissions.None;
+                        break;
+                    case PolygonSubscription.starter:
+                        Permissions =
+                            PolygonPermissions.Snapshots |
+                            PolygonPermissions.WebSockets |
+                            PolygonPermissions.Trades |
+                            PolygonPermissions.Quotes;
+                        break;
+                    default:
+                        Permissions =
+                            PolygonPermissions.None;
+                        break;
+                }
+            }
+        }
+        public bool HasPermission(PolygonPermissions permission)
+        {
+            return Permissions.HasFlag(permission);
         }
     }
+
     public class StocksSettings : AssetClassSettings
     {
         public override DateTime HistoricalDataLookback
         {
             get
             {
-                switch (Subsription)
+                switch (Subscription)
                 {
                     case PolygonSubscription.basic:
                         return DateTime.Today.AddYears(-2);
@@ -68,13 +218,14 @@ namespace PolygonApiClient.MasterClient
         {
         }
     }
+
     public class OptionsSettings : AssetClassSettings
     {
         public override DateTime HistoricalDataLookback
         {
             get
             {
-                switch (Subsription)
+                switch (Subscription)
                 {
                     case PolygonSubscription.basic:
                         return DateTime.Today.AddYears(-2);
@@ -94,13 +245,14 @@ namespace PolygonApiClient.MasterClient
         {
         }
     }
+
     public class CurrenciesSettings : AssetClassSettings
     {
         public override DateTime HistoricalDataLookback
         {
             get
             {
-                switch (Subsription)
+                switch (Subscription)
                 {
                     case PolygonSubscription.basic:
                         return DateTime.Today.AddYears(-2);
@@ -116,13 +268,14 @@ namespace PolygonApiClient.MasterClient
         {
         }
     }
+
     public class IndicesSettings : AssetClassSettings
     {
         public override DateTime HistoricalDataLookback
         {
             get
             {
-                switch (Subsription)
+                switch (Subscription)
                 {
                     case PolygonSubscription.basic:
                         return DateTime.Today.AddYears(0);
@@ -140,5 +293,4 @@ namespace PolygonApiClient.MasterClient
         {
         }
     }
-
 }
